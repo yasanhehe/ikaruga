4d7d8d0b089f86ba727d6f715eff410176e58fe4
大松 生輝 <m21029@g.metro-cit.ac.jp>
2023-12-29 20:39:05 +0900

    Add Ranking component to home page and fix path to Ranking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 // Import ranking component if its done
-// import Ranking from "@/components/home/ranking';
+import Ranking from '../components/home/ranking/Ranking';
 
 const Home = () => {
 	return (
 		<div className="flex flex-col pt-4 sm:ml-[120px] md:ml-[250px] sm:border-r pb-20 min-h-screen w-full">
 			<span className="px-8 mt-10 font-bold text-3xl">Home(Ranking)</span>
+            <Ranking />
 		</div>
 	)
 }
