ed0197b1d8ddce7993fb71a943e72b29eff69d3f
kAwA1290 <m21037@g.metro-cit.ac.jp>
2023-12-31 22:21:56 +0900

    prevent too many redirect error

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,18 +3,27 @@ import { NextRequest, NextResponse } from 'next/server'
 const WHITE_IP = ['39.111.69.183'];
 
 export async function middleware(req: NextRequest) {
+	console.warn('jikkousaretayo!')
+	const path = req.nextUrl.pathname;
 	const res = NextResponse.next();
 
-	let ip: string = req.ip ?? req.headers.get('x-real-ip') ?? '';
+	//too many redirect防止
+	if (path === '/access-denied') {
+		return res;
+	}
 
+	let ip: string = req.ip ?? req.headers.get('x-real-ip') ?? '';
 	const forwardedFor = req.headers.get('x-forwarded-for');
+
 	if (!ip && forwardedFor) {
 		ip = forwardedFor.split(',')[0] ?? 'unknown_ip';
 	}
 
 	if (!WHITE_IP.includes(ip)) {
-		//redirect先はフルパスで指定する事!
-		return NextResponse.redirect(new URL('/access-denied', req.url));
+		//redirect先はFULL PATHで
+		const redirectUrl = new URL('/access-denied', req.url);
+		console.warn(redirectUrl.href);
+		return NextResponse.redirect(redirectUrl.href);
 	}
 
 	return res;
