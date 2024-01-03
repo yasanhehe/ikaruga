5834a087fdbddab969f45ff290d02c4b1872e992
kAwA1290 <m21037@g.metro-cit.ac.jp>
2024-01-03 22:18:40 +0900

    add shohyo page

--- src/app/(bar)/page.tsx
+++ src/app/(bar)/page.tsx
@@ -3,7 +3,7 @@ import Ranking from '@components/home/ranking/Ranking';
 
 const Home = () => {
 	return (
-		<div className="flex flex-col pt-4 sm:ml-[120px] md:ml-[250px] sm:border-r pb-20 min-h-screen">
+		<div className="flex flex-col pt-4 ml-4 sm:ml-[120px] md:ml-[280px] border-r pb-0 min-h-screen">
 			<span className="px-8 mt-10 font-bold text-3xl">Home(Ranking)</span>
             <Ranking />
 		</div>
--- src/app/(bar)/scan/page.tsx
+++ src/app/(bar)/scan/page.tsx
@@ -8,19 +8,24 @@ const Scan = () => {
     const searchUrl = `https://lib.aiit.ac.jp/index.php?action=pages_view_main&active_action=v3search_view_main_init&block_id=296&tab_num=0&op_param=words%3D${searchQuery}%26srhRevTagFlg%3D#v3search_view_main_init`;
     window.location.href = searchUrl;
   };
-	return (
-		/*<div className="flex flex-col pt-4 sm:ml-[120px] md:ml-[250px] sm:border-r sm:border-zinc-700 pb-20 min-h-screen">*/
-		<div className="flex flex-col pt-4 sm:ml-[120px] md:ml-[250px] sm:border-r pb-20 min-h-screen">
-			<span className="px-8 mt-10 font-bold text-3xl">Scan</span>
-      <Scanner
-        onReadCode={(result) => setCodes((codes) => Array.from(new Set([...codes, result.getText()])))}
-         />
-      <textarea value={codes.join('\n')} />
-      <button onClick={() => handleGoogleSearch(codes.join())} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-[20%] ml-20">
-      図書館のページに飛ぶ
+  return (
+    <div className="flex flex-col pt-4 ml-4 sm:ml-[120px] md:ml-[280px] border-r pb-0 min-h-screen">
+      <span className="px-8 mt-10 font-bold text-3xl">Scan</span>
+      <div className="mt-8 mx-auto w-full max-w-md border border-gray-300 rounded-md overflow-hidden shadow-lg"> {/* カメラ画面のスタイル */}
+        <Scanner
+          onReadCode={(result) => setCodes((codes) => Array.from(new Set([...codes, result.getText()])))}
+        />
+      </div>
+      <textarea value={codes.join('\n')} className="mt-4 p-4 border border-gray-300 rounded" />
+      <button
+        onClick={() => handleGoogleSearch(codes.join())}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-[20%] ml-20 mt-4"
+      >
+        図書館のページに飛ぶ
       </button>
-		</div>
-	)
+    </div>
+  );
+
 }
 
 export default Scan;
