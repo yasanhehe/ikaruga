dacd97dcb0217d8cf06dfab77c6afe223a23f87a
kAwA1290 <m21037@g.metro-cit.ac.jp>
2024-01-04 22:54:29 +0900

    test post api

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,34 +2,48 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@lib/prisma';
 
 export async function POST(
-	request: NextApiRequest,
-	response: NextApiResponse
+	request: Request,
 ) {
-	const chunks = [];
-    for await (const chunk of request.body) {
-        chunks.push(chunk);
-    }
-    const body = Buffer.concat(chunks).toString();
-    const { title, isbn, essay } = JSON.parse(body);
-	return response.status(200).json({ title, isbn, essay });
-	//try {
-	//	const data = await prisma.post.create({
-	//		data: {
-	//			title,
-	//			isbn,
-	//			essay
-	//		},
-	//		select: {
-	//			id: true,
-	//			title: true,
-	//			isbn: true,
-	//			essay: true
-	//		}
-	//	});
-	//	return response.status(200).json({ data });
-	//} catch (err) {
-	//	console.warn(err);
-	//	return response.status(500).json({ err });
-	//}
+	const readableStreamText = await new Response(request.body).text();
+	const { title, isbn, essay } = JSON.parse(readableStreamText);
+	const response = new Response(JSON.stringify({ title, isbn, essay }), {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	return response;
 }
 
+//export async function POST(
+//	request: NextApiRequest,
+//	response: NextApiResponse
+//) {
+//	const chunks = [];
+//    for await (const chunk of request.body) {
+//        chunks.push(chunk);
+//    }
+//    const body = Buffer.concat(chunks).toString();
+//    const { title, isbn, essay } = JSON.parse(body);
+//	return response.status(200).json({ title, isbn, essay });
+//	//try {
+//	//	const data = await prisma.post.create({
+//	//		data: {
+//	//			title,
+//	//			isbn,
+//	//			essay
+//	//		},
+//	//		select: {
+//	//			id: true,
+//	//			title: true,
+//	//			isbn: true,
+//	//			essay: true
+//	//		}
+//	//	});
+//	//	return response.status(200).json({ data });
+//	//} catch (err) {
+//	//	console.warn(err);
+//	//	return response.status(500).json({ err });
+//	//}
+//}
+//
