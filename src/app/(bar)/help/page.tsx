33d6ec52f7aa81b9e060b75f9a9604131a612f82
kAwA1290 <m21037@g.metro-cit.ac.jp>
2024-01-23 13:33:13 +0900

    add help page

--- src/app/(bar)/help/page.tsx
+++ src/app/(bar)/help/page.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 import BookReview from "@components/read-share/book-review";
 import review_test from "@data/review_test.json";
 
+const ReadShare = () => {
 	return (
 		<div className="flex flex-col pt-4 ml-4 md:ml-[280px] pb-0 min-h-screen">
 			<span className="px-8 mt-10 font-bold text-3xl">ReadShare</span>
