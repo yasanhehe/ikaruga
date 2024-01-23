16c1f9829b8f36cb5a121ab5c523d517bac757a0
kAwA1290 <m21037@g.metro-cit.ac.jp>
2024-01-23 10:44:41 +0900

    small fix for title

--- src/app/(bar)/page.tsx
+++ src/app/(bar)/page.tsx
@@ -3,8 +3,8 @@ import Ranking from '@components/home/ranking/Ranking';
 
 const Home = () => {
 	return (
-		<div className="flex flex-col w-full pb-20">
-			<span className="mt-10 font-bold md:ml-[280px] text-3xl">Ranking</span>
+		<div className="flex flex-col pt-4 ml-4 w-full pb-20">
+			<span className="px-8 mt-10 font-bold md:ml-[280px] text-3xl">Ranking</span>
 			<Ranking />
 		</div>
 	)
